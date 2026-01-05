2cdf811f3c3c8375ac39f2ed8b2f9cd0860c9471
marcossamueldev <marcossamuelps.dev@gmail.com>
2026-01-05 09:42:39 -0300

    add interatividade com js
    utilizando o js para criar os produtos dinamicamente preparando o html

--- estudosiniciais/site-cafeteria/script.js
+++ estudosiniciais/site-cafeteria/script.js
@@ -1,1 +1,46 @@
-console.log("Script carregado com sucesso!");
+const produtos = [
+    {
+        id: 1,
+        nome: "Café Expresso",
+        preço: 15.99,
+        preçoAntigo: 20.99,
+        imagem: "./img/menu-1.png"
+    },
+    {
+        id: 2,
+        nome: "Cappuccino",
+        preço: 19.99,
+        preçoAntigo: 22.99,
+        imagem: "./img/menu-2.png"
+    },
+    {
+        id: 3,
+        nome: "Latte",
+        preço: 18.99,
+        preçoAntigo: 23.99,
+        imagem: "./img/menu-3.png"
+    },
+    {
+        id: 4,
+        nome: "Cafe-Latte",
+        preço: 18.99,
+        preçoAntigo: 23.99,
+        imagem: "./img/menu-4.png"
+    },{
+        id: 5,
+        nome: "Cafe torrado",
+        preço: 18.99,
+        preçoAntigo: 23.99,
+        imagem: "./img/menu-5.png"
+    },{
+        id: 6,
+        nome: "cafe gelado",
+        preço: 18.99,
+        preçoAntigo: 23.99,
+        imagem: "./img/menu-6.png"
+    },
+];
+
+console.log(produtos);
+
+const menuContainer = document.getElementById("menu-container")
